d1e15d0b5e179f41eb78a4b62f44d961d1b0e136
westwe6 <westwe6@users.noreply.github.com>
2015-10-26 12:24:55 -0400

    Create trimmomaticPE_QC.sh
    #Your next step is to improve the quality of your samples using trimmomatic in a bash shell script.
    #Make sure the 18S V9 primers (Fluidigm-PE.fa) are included in the fastq18S directory (this can be done using nano or filezilla).
    #Creates a shell script called trimmomaticPE_QC.sh, this shell will be used to quality score (minimum 20 base Q-score accepted, and minimum read length of 125)
    #The script also removes adapters, primer dimers, and flips reads to make sure they are able to be paired.
    #To do this type the line nano trimmomaticPE_QC.sh and then input the following lines including, but ending with 'done':

---
+++ trimmomaticPE_QC.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+	
+	for file in $(<filelist_PE_QC.txt)
+do
+	java -jar $TRIM/trimmomatic  PE -threads 4 -trimlog mv ${file}_QC_PE_log.fasta ${file}_L001_R1_001.fastq ${file}_L001_R2_001.fastq ${file}_L001_R1_001_pTrim.fastq ${file}_L001_R1_001_uTrim.fastq ${file}_L001_R2_001_pTrim.fastq ${file}_L001_R2_001_uTrim.fastq ILLUMINACLIP:Fluidigm-PE.fa:2:30:12 LEADING:20 TRAILING:20 MINLEN:125
+
+done
