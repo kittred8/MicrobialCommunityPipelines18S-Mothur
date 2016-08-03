0feacee1f2547e611507bae85deea152bd22f3dd
kittred8 <kittred8@msu.edu>
2016-08-03 10:53:19 -0400

    Update trimmomaticPE_QC.sh

--- trimmomaticPE_QC.sh
+++ trimmomaticPE_QC.sh
@@ -2,6 +2,6 @@
 	
 	for file in $(<filelist_PE_QC.txt)
 do
-	java -jar $TRIM/trimmomatic  PE -threads 4 -trimlog mv ${file}_QC_PE_log.fasta ${file}_L001_R1_001.fastq ${file}_L001_R2_001.fastq ${file}_L001_R1_001_pTrim.fastq ${file}_L001_R1_001_uTrim.fastq ${file}_L001_R2_001_pTrim.fastq ${file}_L001_R2_001_uTrim.fastq ILLUMINACLIP:Fluidigm-PE.fa:2:30:12 LEADING:20 TRAILING:20 MINLEN:125
+	java -jar $TRIM/trimmomatic  PE -threads 4 -trimlog ${file}_QC_PE_log.fasta ${file}_L001_R1_001.fastq ${file}_L001_R2_001.fastq ${file}_L001_R1_001_pTrim.fastq ${file}_L001_R1_001_uTrim.fastq ${file}_L001_R2_001_pTrim.fastq ${file}_L001_R2_001_uTrim.fastq ILLUMINACLIP:Fluidigm-PE.fa:2:30:12 LEADING:20 TRAILING:20 MINLEN:125
 
 done
